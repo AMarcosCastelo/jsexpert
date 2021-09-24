19b84c56aa92707cd03e789ed8d92ece5952c87b
Antonio Marcos <marcos-12marcos@hotmail.com>
2021-09-24 06:38:11 -0300

    class08/esmodules

---
+++ modulo3/demo02-esmodules-internationalization/test/person.test.js
@@ -0,0 +1,38 @@
+import mocha from 'mocha';
+const { describe, it } = mocha;
+import chai from 'chai';
+const { expect } = chai;
+import Person from '../src/person.js';
+
+describe('Person', () => {
+  it('should return a person instance from a string', () => {
+    const person = Person.generateInstanceFromString(
+      '1 Bike,Carro 20000 2020-01-01 2021-01-01'
+    );
+    const expected = {
+      from: '2020-01-01',
+      to: '2021-01-01',
+      vehicles: ['Bike', 'Carro'],
+      kmTraveled: "20000",
+      id: '1'
+    };
+
+    expect(person).to.be.deep.equal(expected);
+  });
+
+  it('should return formatted values', () => {
+    const person = Person.generateInstanceFromString(
+      '1 Bike,Carro 20000 2020-01-01 2021-01-01'
+    );
+    const result = person.formatted('pt-BR');
+    const expected = {
+      id: 1,
+      vehicles: 'Bike e Carro',
+      kmTraveled: '20.000 km',
+      from: '01 de janeiro de 2020',
+      to: '01 de janeiro de 2021'
+    };
+
+    expect(result).to.be.deep.equal(expected);
+  });
+});
